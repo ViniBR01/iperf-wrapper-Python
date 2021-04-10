44b9216dd1b34f692dacf0c507de0d52004c9942
ViniBR01 <vinicius@rice.edu>
2021-04-10 17:31:24 -0500

    Wrap script in a main function with argv

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import time
 import random
+import sys, getopt
 
 def run_iperf(server_ip, filesize):
     return subprocess.check_output(["iperf3", "-c", server_ip, "-J", "-M 1460", "-n "+str(filesize)])
@@ -13,15 +14,19 @@ def extract_bps(json_out):
 def get_interval(min_interval, max_interval):
     return random.random()*(max_interval - min_interval) + min_interval
 
-size = 100000
-min_interval = 0.5
-max_interval = 1.5
-t_end = time.time() + 10
-server_ip = "192.168.0.102"
+def main(argv):
+    size = 100000
+    min_interval = 0.5
+    max_interval = 1.5
+    t_end = time.time() + 10
+    server_ip = "192.168.0.102"
 
-while time.time() < t_end:
-    interval = get_interval(min_interval, max_interval)
-    time.sleep(interval)
-    json_out = run_iperf(server_ip, size)
-    mbps_out = extract_bps(json_out)/1024/1024
-    print(mbps_out)
+    while time.time() < t_end:
+        interval = get_interval(min_interval, max_interval)
+        time.sleep(interval)
+        json_out = run_iperf(server_ip, size)
+        mbps_out = extract_bps(json_out)/1024/1024
+        print(mbps_out)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
