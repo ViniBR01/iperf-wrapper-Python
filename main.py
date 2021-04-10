8e8380a9efae60d91b4edffa683a57d9cebef8e1
ViniBR01 <vinicius@rice.edu>
2021-04-10 17:48:12 -0500

    Add command line argument parsing

--- main.py
+++ main.py
@@ -20,6 +20,28 @@ def main(argv):
     max_interval = 1.5
     t_end = time.time() + 10
     server_ip = "192.168.0.102"
+    
+    try:
+        opts, args = getopt.getopt(argv,"hs:l:i:t:g:",["file-size=","length=","server-ip=","max-interval=","min-interval="])
+    except getopt.GetoptError:
+        print('main.py -i <server-ip> -s <file-size> -l <time-length> -t <max-interval> -g <min-interval>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -i <server-ip> -s <file-size> -l <time-length> -t <max-interval> -g <min-interval>')
+            sys.exit()
+        elif opt in ("-s", "--file-size"):
+            size = int(arg)
+        elif opt in ("-l", "--length"):
+            t_end = time.time() + int(arg)
+        elif opt in ("-t", "--max-interval"):
+            max_interval = float(arg)
+        elif opt in ("-g", "--min-interval"):
+            min_interval = float(arg)
+        elif opt in ("-i", "--server-ip"):
+            server_ip = arg
+
+    # print("fileSize = " + str(size))
 
     while time.time() < t_end:
         interval = get_interval(min_interval, max_interval)
