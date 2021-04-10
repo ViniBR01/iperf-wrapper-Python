87940da4e58557e546557bcd82beb4e5ba0d79e3
ViniBR01 <vinicius@rice.edu>
2021-04-10 14:58:00 -0500

    Refactor to use functions

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import subprocess
 import json
 
-json_out = subprocess.check_output(["iperf3", "-c", "192.168.0.102", "-M 1460", "-n 146000", "-J"])
+def run_iperf(server_ip, filesize):
+    return subprocess.check_output(["iperf3", "-c", server_ip, "-J", "-M 1460", "-n "+str(filesize)])
 
-dict_out = json.loads(json_out)
-bps_out = dict_out["end"]["streams"][0]["receiver"]["bits_per_second"]
-print(bps_out/1024/1024)
+json_out = run_iperf("192.168.0.102", 146000)
+
+def extract_bps(json_out):
+    dict_out = json.loads(json_out)
+    return dict_out["end"]["streams"][0]["receiver"]["bits_per_second"]
+
+mbps_out = extract_bps(json_out)/1024/1024
+print(mbps_out)
